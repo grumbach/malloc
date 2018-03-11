30649952ff1a67edcf080feefbaaaa30991254df
grumbach <agrumbac@student.42.fr>
2018-03-11 17:29:03 +0100

    [CLEAN] cleaned minimap and README

--- README.md
+++ README.md
@@ -1,12 +1,42 @@
-# malloc
+# Malloc
+
 This project is about implementing a dynamic memory allocation mechanism.
 
-# usage
+<img src="./minimap.png"/>
+Screenshot of the memory minimap feature.
 
-loading libft_malloc.so :
+# Usage
+
+### Getting it
+
+```bash
+$> git clone https://github.com/grumbach/malloc && cd malloc && make
+```
+
+### Running programs using libft_malloc.so
+
+```bash
+$> ./run.sh ls #or any other program
+```
+
+### Using libft_malloc.so in lldb on macOS
+
+```js
+(lldb) env DYLD_LIBRARY_PATH=.
+(lldb) env DYLD_INSERT_LIBRARIES="libft_malloc.so"
+(lldb) env DYLD_FORCE_FLAT_NAMESPACE=1
+```
+
+# Features
 
 ```c
-env DYLD_LIBRARY_PATH=.
-env DYLD_INSERT_LIBRARIES="libft_malloc.so"
-env DYLD_FORCE_FLAT_NAMESPACE=1
+void	free(void *ptr);
+void	*malloc(size_t size);
+void	*calloc(size_t count, size_t size);
+void	*realloc(void *ptr, size_t size);
+void	*reallocf(void *ptr, size_t size);
+
+void	show_alloc_mem();
+void	show_alloc_mem_hex(void *ptr);
+void	show_alloc_mem_minimap();
 ```
--- srcs/minimap.c
+++ srcs/minimap.c
@@ -6,7 +6,7 @@
 /*   By: agrumbac <agrumbac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/08 20:41:49 by agrumbac          #+#    #+#             */
-/*   Updated: 2018/03/10 23:52:41 by agrumbac         ###   ########.fr       */
+/*   Updated: 2018/03/11 16:57:57 by agrumbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,10 @@ static inline void	print_mem(const char *mem_state, const size_t size)
 		j = 0;
 		while (j < MALLOC_MINIMAP)
 		{
-			if (*(mem_state + i * MALLOC_MINIMAP + j) == '.')
-				ft_printf("%s%c%s", "\e[42;30m", ' ', "\e[0m");
+			if (*(mem_state + i * MALLOC_MINIMAP + j))
+				ft_printf("%s%c%s", "\e[41m", ' ', "\e[0m");
 			else
-				ft_printf("%s%c%s", "\e[41;34m", ' ', "\e[0m");
+				ft_printf("%s%c%s", "\e[42m", ' ', "\e[0m");
 			j++;
 		}
 		i++;
@@ -43,16 +43,15 @@ static void			draw_mem(t_malloc_mem *mem, const size_t size)
 	t_malloc_chunk	*chunk;
 	int				chunk_pos;
 
-	ft_printf("\n[Zone starting at %s%p%s of %s%lu%s bytes]",\
+	ft_printf("\n%s[Zone starting at %s%p%s of %s%lu%s bytes]", "\e[32m", \
 		"\e[33m", mem, "\e[32m", "\e[34m", size * MALLOC_ZONE, "\e[32m");
-	ft_memset(mem_state, '.', MALLOC_ZONE * MALLOC_MINIMAP);
-	mem_state[MALLOC_ZONE * MALLOC_MINIMAP] = '\0';
+	ft_bzero(mem_state, MALLOC_ZONE * MALLOC_MINIMAP + 1);
 	chunk = mem->alloc;
 	while (chunk)
 	{
 		chunk_pos = ((void*)chunk - ((void*)mem + sizeof(t_malloc_mem))) / \
 			(size + sizeof(t_malloc_chunk));
-		ft_memset((char*)mem_state + chunk_pos * MALLOC_MINIMAP, 'W', \
+		ft_memset((char*)mem_state + chunk_pos * MALLOC_MINIMAP, 42, \
 			chunk->size * MALLOC_MINIMAP / size);
 		chunk = chunk->next;
 	}
